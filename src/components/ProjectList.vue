d572c3b775f0aefff8ee1ca4d3473843105967d1
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-06-10 18:17:30 +0200

    feat: milestone 3-4

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- cards -->
+                     <div class="card" v-for="item in items" :key="item.id">
+                        
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProjectList"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
