2c6456571a3f78ac8f3657450d51cd0fe327ca78
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-06-12 10:59:30 +0200

    fix: img alt

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,7 +4,7 @@
     <!-- cards -->
     <div class="card my-3" style="height: 18rem;">
         <div class="card-body d-flex flex-column justify-content-between flex-wrap">
-            <img :src="`http://127.0.0.1:8000/storage/${projectInfo.cover_img}`" alt="{{ projectInfo.name }}" style="height: 5rem; width: fit-content;">
+            <img :src="`http://127.0.0.1:8000/storage/${projectInfo.cover_img}`" :alt="projectInfo.name" style="height: 5rem; width: fit-content;">
             <h5 class="card-title">{{ projectInfo.name }}</h5>
             <div v-if="projectInfo.type">
                 <strong>Type</strong>:
